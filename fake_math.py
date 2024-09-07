8c33349f2cb7d5e939b9a927afb6b312d194db1d
YuriSherin <YuriSherin@mail.ru>
2024-09-08 02:18:22 +0700

    Create project

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+def divide(first, second):
+    if second != 0:
+        return first / second
+    else:
+        return 'Ошибка'
+
+if __name__ == '__main__':
+    pass
---
+++ main.py
@@ -0,0 +1,13 @@
+from fake_math import divide as f_divide
+from  true_math import divide as t_divide
+
+
+if __name__ == '__main__':
+    result1 = f_divide(69, 3)
+    result2 = f_divide(3, 0)
+    result3 = t_divide(49, 7)
+    result4 = t_divide(15, 0)
+    print(result1)
+    print(result2)
+    print(result3)
+    print(result4)
